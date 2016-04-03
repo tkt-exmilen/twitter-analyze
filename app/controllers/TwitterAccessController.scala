9356e354ff6b1965c76e48a3eb9b00ae292b2d2a
tkt0821 <tkt0821@gmail.com>
2016-04-03 16:12:09 +0900

    twitter検索と検索結果の表示を追加

--- app/controllers/TwitterAccessController.scala
+++ app/controllers/TwitterAccessController.scala
@@ -5,13 +5,21 @@ import twitter4j._
 import twitter4j.auth._
 import play.api.cache._
 import play.api.Play.current
+import play.api.cache
+import twitter4j.conf.ConfigurationBuilder
 
 
 class TwitterAccessController extends Controller {
+
+  var twitter: Twitter = null
+
   def search = Action { request =>
     val params: Map[String, Seq[String]] = request.queryString
-    val hashtag = params("hashtag").head
-    Ok(<p>{hashtag}</p>).as(HTML)
+    val keyword = params("keyword").head
+    val query: Query = new Query()
+    query.setQuery(keyword)
+    val result: QueryResult = twitter.search(query)
+    Ok(<p>{result.getTweets().toString()}</p>).as(HTML)
   }
 
   def twitterLogin = Action { request =>
@@ -33,13 +41,19 @@ class TwitterAccessController extends Controller {
             val getRequestToken: Option[RequestToken] = Cache.getAs[RequestToken]("requestToken")
             getRequestToken match {
               case Some(requestToken) => {
+                println(request)
                 var authToken: String = request.queryString.get("oauth_token").get.head
                 var authVerifier: String = request.queryString.get("oauth_verifier").get.head
                 twitter.getOAuthAccessToken(requestToken, authVerifier)
                 var user: User = twitter.showUser(twitter.getId())
                 Cache.set("twitter_user", user, 4320)
+                Cache.set("twitter_authToken", authToken, 4320)
+                Cache.set("twitter_authVerifier", authVerifier, 4320)
                 Cache.remove("twitter")
                 Cache.remove("requestToken")
+
+                this.twitter = twitter
+
               }
               case _ =>
             }
